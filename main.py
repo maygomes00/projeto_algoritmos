1666de6160c4158a2645dfe07652c9d99245dc18
maygomes00 <mgop@cin.ufpe.br>
2024-07-08 21:52:20 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from scripts.database import database
+from scripts.grafo import dijkstra, initialize_graph
+#from scripts.visualization import visualizar_Graforafo
+
+if __name__ == "__main__":
+    #Carregar dados do database
+    database()
+    #Inicializar grafo
+    Grafo = initialize_graph()
+
+    origem = int(input("Digite o nó de origem: "))
+    destino = int(input("Digite o nó de destino: "))
+    #Chamar a função Dijkstra para calcular o caminho mais curto e as distâncias do nó de origem ao nó de destino
+    caminho, distancias = dijkstra(Grafo, origem, destino)
+
+    if distancias[destino] == float('inf'):
+        print(f"Não há caminho disponível de {origem} para {destino}.")
+    else:
+        # Exibe o caminho mais curto
+        print(f"O caminho mais curto de {origem} para {destino} é: {caminho}")
+        
+        # Exibe a distância total do caminho mais curto
+        print(f"A distância total é: {distancias[destino]}")
